434bdd38c471f93f896037472d6ffa5e00994374
yakiimokko <ykm.rad.creep.s11@gmail.com>
2022-06-15 02:03:09 +0000

    array

---
+++ js/array.js
@@ -0,0 +1,11 @@
+const userName = ['侍太郎', '侍一郎', '侍二郎', '侍三郎', '侍四郎'];
+
+console.log(userName);
+
+userName[1] = '侍花子';
+
+userName[5] = '侍五郎';
+
+console.log(userName);
+
+console.log(userName[2]);
