f2174d59101fed54eab46843927860c483b84084
yakiimokko <ykm.rad.creep.s11@gmail.com>
2022-06-15 02:13:42 +0000

    function.js

---
+++ js/function.js
@@ -0,0 +1,14 @@
+const sayGoodMorning =()=>{
+    console.log('おはようございます！');
+    console.log('よく眠れましたか？');
+    console.log('今日も一日がんばりましょう！');
+}
+
+const sayGoodEvening =()=>{
+    console.log('こんばんは！');
+    console.log('今日も一日お疲れさまでした。');
+}
+
+sayGoodMorning();
+
+sayGoodEvening();
