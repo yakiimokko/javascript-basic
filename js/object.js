0fb9a94888e6c1b94feb41790a750b35149e7a8a
yakiimokko <ykm.rad.creep.s11@gmail.com>
2022-06-15 02:07:09 +0000

    object.js

---
+++ js/object.js
@@ -0,0 +1,11 @@
+const personalDate = {name: '侍太郎', age: 36, gender: '男性'};
+
+console.log(personalDate);
+
+personalDate.age = 37;
+
+personalDate.address = '東京都';
+
+console.log(personalDate);
+
+console.log(personalDate.gender);
