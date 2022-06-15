d06cff5bf7a41205b06be7b44849e1bb66b190be
yakiimokko <ykm.rad.creep.s11@gmail.com>
2022-06-15 01:20:37 +0000

    switch

--- js/if-switch.js
+++ js/if-switch.js
@@ -30,3 +30,28 @@
  else {
    console.log('はずれです');
  }
+
+ // 変数numに0～4までのランダムな整数を代入する
+ num = Math.floor(Math.random() * 5);
+ 
+ //変数numの値を出力する
+ console.log(num);
+ 
+ //変数numの値によって出力する文字列を切り替える
+ switch (num) {
+     case 2:
+          console.log('小吉です');
+          break;
+     
+     case 3:
+          console.log('中吉です');
+          break;
+         
+     case 4:
+          console.log('大吉です');
+          break;
+          
+     default:
+          console.log('末吉です')
+          break;
+ }
