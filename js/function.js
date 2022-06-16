32631720b8d424781be4ecb577d18264cb2a9434
yakiimokko <ykm.rad.creep.s11@gmail.com>
2022-06-16 13:44:08 +0000

    function.js

--- js/function.js
+++ js/function.js
@@ -12,3 +12,24 @@ const sayGoodEvening =()=>{
 sayGoodMorning();
 
 sayGoodEvening();
+
+//引数
+const calculateTotal =(price)=> {
+    console.log(price + 500 +'円');
+}
+
+calculateTotal(1200);
+
+const addTwoArguments = (price, shippingFee) => {  
+   console.log(price + shippingFee + '円');
+ }
+ 
+addTwoArguments(1200, 500);
+
+ // 与えられた引数numを2倍にし、その値を戻り値として返す関数を定義する
+ const double = (num) => {
+   return num * 2;
+ }
+ 
+ // 関数の戻り値を出力する
+ console.log(double(30));
