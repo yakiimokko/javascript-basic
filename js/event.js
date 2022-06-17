804c13a92d4813f9de16d27d1806b2cef8b14627
yakiimokko <ykm.rad.creep.s11@gmail.com>
2022-06-17 00:26:41 +0000

    event.js

--- js/event.js
+++ js/event.js
@@ -15,3 +15,32 @@ addBtn.addEventListener('click', () => {
     childList.textContent ='リストが追加されました';
     parentList.appendChild(childList);
 });
+
+//const-btnというidを持つHTML要素を取得し、定数に代入する
+const countBtn = document.getElementById('count-btn');
+
+//HTML要素がクリックされたときにイベント処理を実行する
+countBtn.addEventListener('click', () => {
+    const text = document.forms.textForm.textBox.value;
+    console.log(text.length + '文字');
+});
+
+
+const areaBtn = document.getElementById('area-btn');
+
+areaBtn.addEventListener('click' , ()=> {
+    const area = document.forms.areaForm.area.value;
+    console.log(area);
+});
+
+
+const osBtn = document.getElementById('os-btn');
+
+osBtn.addEventListener('click' , () =>{
+    const items = document.forms.osForm.os;
+    for (let i = 0; i < items.length; i++) {
+        if (items[i].checked){
+        console.log(items[i].value);
+        }
+    };
+});
